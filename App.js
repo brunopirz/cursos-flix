6d8a9e37d387e92d472d3b85c7528db408cd66de
brunopirz <156872746+brunopirz@users.noreply.github.com>
2025-03-11 13:50:05 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+// frontend/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import AdminDashboard from './components/AdminDashboard';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        {/* Outras rotas, como a área de membros, podem ser adicionadas aqui */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
